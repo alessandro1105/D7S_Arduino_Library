44a9853b87b9712473b013a211157acc6804a4ff
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2019-02-24 18:51:30 +0100

    Updated SeismographWithInterrupt example
    - Updated SeismographWithInterrupt example due to internal library changes

--- examples/SeismographWithInterrupt/SeismographWithInterrupt.ino
+++ examples/SeismographWithInterrupt/SeismographWithInterrupt.ino
@@ -1,5 +1,5 @@
 /*
-   Copyright 2017 Alessandro Pasqualini
+   Copyright 2019 Alessandro Pasqualini
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
@@ -21,63 +21,79 @@
 
 #include <D7S.h>
 
-// Fishino32 interrupt pins
-#if defined(_FISHINO32_)
+#if defined(_FISHINO32_) // Fishino32 interrupt pins
   #define INT1_PIN 3 //interrupt pin INT1 of D7S attached to pin 3 of Fishino32
   #define INT2_PIN 5 //interrupt pin INT2 of D7S attached to pin 5 of Fishino32
-// Esp8266 interrupt pins (tested on WeMos D1 R1)
-#elif defined(ESP8266)
+  #define INT2_CHANGING false //interrupt pin INT2 capable of handling CHANGE event
+
+#elif defined(ESP8266) // Esp8266 interrupt pins (tested on WeMos D1 R1)
   #define INT1_PIN D7 //interrupt pin INT1 of D7S attached to pin D7 of ESP8266
   #define INT2_PIN D8 //interrupt pin INT2 of D7S attached to pin D8 of ESP8266
-// Arduino UNO/Fishino UNO interrupt pins
-#else
+  #define INT2_CHANGING true //interrupt pin INT2 capable of handling CHANGE event
+
+#elif defined(ESP32) // Esp8266 interrupt pins (tested on WeMos D1 R1)
+  #define INT1_PIN 13 //interrupt pin INT1 of D7S attached to pin D7 of ESP8266
+  #define INT2_PIN 15 //interrupt pin INT2 of D7S attached to pin D8 of ESP8266
+  #define INT2_CHANGING true //interrupt pin INT2 capable of handling CHANGE event  
+
+#else // Arduino UNO/Fishino UNO interrupt pins
   #define INT1_PIN 2 //interrupt pin INT1 of D7S attached to pin 2 of Arduino
   #define INT2_PIN 3 //interrupt pin INT2 of D7S attached to pin 3 of Arduino
+  #define INT2_CHANGING true //interrupt pin INT2 capable of handling CHANGE event
 #endif
+
+// Varibale to handle printing of earthqauke readings
+bool occuring = false; // Shows if an earthqauke is occuring
+bool printed = true; // Shows if the information of the earthquake have been printed
 
 
 //--- EVENT HANDLERS --
 //function to handle the start of an earthquake
 void startEarthquakeHandler() {
   Serial.println("-------------------- EARTHQUAKE STARTED! --------------------\n");
+
+  // Earthquake is occuring
+  occuring = true;
+
+  //Do not call any D7S methods inside this function, because D7S library uses Wire,
+  //which is based on interrupts internally, and it could result in a deadlock due to interrupts
+  //priorities. In most platforms Wire has a lower priority over user defined interrupts, so deadlock.
 }
 
-//function to handle the end of an earthquake
-void endEarthquakeHandler(float si, float pga, float temperature) {
-  Serial.println("-------------------- EARTHQUAKE ENDED! --------------------");
-  //printing the SI
-  Serial.print("\tSI: ");
-  Serial.print(si);
-  Serial.println(" [m/s]");
+// //function to handle the end of an earthquake
+void endEarthquakeHandler() {
+  Serial.println("-------------------- EARTHQUAKE ENDED! --------------------\n");
 
-  //printing the PGA
-  Serial.print("\tPGA (Peak Ground Acceleration): ");
-  Serial.print(pga);
-  Serial.println(" [m/s^2]");
+  // Earthquake is ended
+  occuring = false;
+  // information must be printed
+  printed = false;
 
-  //printing the temperature at which the earthquake has occured
-  Serial.print("\tTemperature: ");
-  Serial.print(temperature);
-  Serial.println(" [°C]\n");
-
-  //reset earthquake events
-  D7S.resetEvents();
+  //Do not call any D7S methods inside this function, because D7S library uses Wire,
+  //which is based on interrupts internally, and it could result in a deadlock due to interrupts
+  //priorities. In most platforms Wire has a lower priority over user defined interrupts, so deadlock.
 }
 
 //function to handle shutoff event
 void shutoffHandler() {
   //put here the code to handle the shutoff event
-  Serial.println("-------------------- SHUTOFF! --------------------\n");
-  Serial.println("Shutting down all device!");
-  //stop all device
-  while (1)
-    ;
+  Serial.println("-------------------- SHUTOFF! --------------------");
+  Serial.println("Better shutting off all electronic devices!\n");
+
+  //Do not call any D7S methods inside this function, because D7S library uses Wire,
+  //which is based on interrupts internally, and it could result in a deadlock due to interrupts
+  //priorities. In most platforms Wire has a lower priority over user defined interrupts, so deadlock.
 }
 
 //function to handle collapse event
 void collapseHandler() {
   //put here the code to handle the collapse event
-  Serial.println("-------------------- COLLAPSE! --------------------\n");
+  Serial.println("-------------------- COLLAPSE! --------------------");
+  Serial.println("Better shutting off all electronic devices!\n");
+
+  //Do not call any D7S methods inside this function, because D7S library uses Wire,
+  //which is based on interrupts internally, and it could result in a deadlock due to interrupts
+  //priorities. In most platforms Wire has a lower priority over user defined interrupts, so deadlock.
 }
 
 
@@ -105,11 +121,8 @@ void setup() {
   D7S.setAxis(SWITCH_AT_INSTALLATION);
 
   //--- INTERRUPT SETTINGS ---
-  //enabling interrupt INT1
-  D7S.enableInterruptINT1(INT1_PIN);
-  //enabling interrupt INT2
-  D7S.enableInterruptINT2(INT2_PIN);
-
+  // Attach interrupts
+  D7S.attachInterruptHandling(INT1_PIN, INT2_PIN, INT2_CHANGING);
   //registering event handler
   D7S.registerInterruptEventHandler(START_EARTHQUAKE, &startEarthquakeHandler); //START_EARTHQUAKE event handler
   D7S.registerInterruptEventHandler(END_EARTHQUAKE, &endEarthquakeHandler); //END_EARTHQUAKE event handler
@@ -117,7 +130,7 @@ void setup() {
   D7S.registerInterruptEventHandler(COLLAPSE_EVENT, &collapseHandler); //COLLAPSE_EVENT event handler
 
   //--- INITIALIZZATION ---
-  Serial.println("Initializing the D7S sensor in 2 seconds. Please keep it steady during the initializing process.");
+  Serial.println("Initializing the D7S sensor in 2 seconds. Please keep it steady during the initialization process.");
   delay(2000);
   Serial.print("Initializing...");
   //start the initial installation procedure
@@ -129,18 +142,12 @@ void setup() {
   }
   Serial.println("INITIALIZED!");
 
-  //--- CHECKING FOR PREVIUS COLLAPSE ---
-  //check if there there was a collapse (if this is the first time the D7S is put in place the installation data may be wrong)
-  if (D7S.isInCollapse()) {
-    collapseHandler();
-  }
-
   //--- RESETTING EVENTS ---
   //reset the events shutoff/collapse memorized into the D7S
   D7S.resetEvents();
 
   //--- STARTING INTERRUPT HANDLING ---
-  D7S.startInterruptHandling();
+  D7S.startInterruptListening();
 
   //--- READY TO GO ---
   Serial.println("\nListening for earthquakes!");
@@ -148,5 +155,26 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (!occuring && !printed) {
+    //printing the SI
+    Serial.print("\tSI: ");
+    Serial.print(D7S.getLastestSI(1));
+    Serial.println(" [m/s]");
+
+    //printing the PGA
+    Serial.print("\tPGA (Peak Ground Acceleration): ");
+    Serial.print(D7S.getLastestPGA(1));
+    Serial.println(" [m/s^2]");
+
+    //printing the temperature at which the earthquake has occured
+    Serial.print("\tTemperature: ");
+    Serial.print(D7S.getLastestTemperature(1));
+    Serial.println(" [°C]\n");
+
+    // Reset events
+    D7S.resetEvents();
+
+    printed = true;
+  }
 }
